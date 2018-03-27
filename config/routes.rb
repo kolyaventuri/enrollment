594b9b3760115e26c3959914399fba0b6c055ae9
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 14:35:31 -0600

    Add address test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :students
+  resources :students do
+    resources :addresses
+  end
 
   root to: 'home#index'
 end
