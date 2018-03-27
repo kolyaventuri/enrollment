35ca84dbd03d42c75651ae5e403f4cd3259df85d
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 14:41:26 -0600

    Restricted address routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :students do
-    resources :addresses
+    resources :addresses, only: [:create, :new]
   end
 
   root to: 'home#index'
