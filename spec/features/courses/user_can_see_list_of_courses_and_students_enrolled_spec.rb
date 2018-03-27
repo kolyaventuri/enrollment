280c9fa945882fd4a85db3a5801f256d430cd599
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 15:51:13 -0600

    Courses routes tests

--- spec/features/courses/user_can_see_list_of_courses_and_students_enrolled_spec.rb
+++ spec/features/courses/user_can_see_list_of_courses_and_students_enrolled_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'User' do
   describe 'visits course index' do
-    it 'should see a list of courses and enrolled students' do
+    it 'should see a list of courses' do
       student = Student.create!(name: 'Bob')
       student2 = Student.create!(name: 'Joe')
 
@@ -22,20 +22,42 @@ describe 'User' do
       expect(page).to have_content(course2.code)
       expect(page).to have_content(course3.code)
 
-      within('#course_1') do
-        expect(page).to have_content(student.name)
-        expect(page).to_not have_content(student2.name)
-      end
+      find('#course_1').click
 
-      within('#course_2') do
-        expect(page).to have_content(student.name)
-        expect(page).to have_content(student2.name)
-      end
+      expect(current_path).to eq(course_path(course1))
+    end
+  end
 
-      within('#course_3') do
-        expect(page).to have_content(student2.name)
-        expect(page).to_not have_content(student.name)
-      end
+  describe 'visits a specific course page' do
+    it 'should only show the students in that course' do
+      student = Student.create!(name: 'Bob')
+      student2 = Student.create!(name: 'Joe')
+
+      course1 = Course.create!(code: 'EEE100', name: 'Intro to Electrical Engineering')
+      course2 = Course.create!(code: 'ENG101', name: 'College Writing')
+      course3 = Course.create!(code: 'MAT202', name: 'College Math')
+      StudentCourse.create!(student_id: student.id, course_id: course1.id)
+
+      StudentCourse.create!(student_id: student.id, course_id: course2.id)
+      StudentCourse.create!(student_id: student2.id, course_id: course2.id)
+
+      StudentCourse.create!(student_id: student2.id, course_id: course3.id)
+
+      visit course_path(course1)
+
+      expect(page).to have_content(student.name)
+      expect(page).to_not have_content(student2.name)
+
+      visit course_path(course2)
+
+      expect(page).to have_content(student.name)
+      expect(page).to have_content(student2.name)
+
+      visit course_path(course3)
+
+      expect(page).to_not have_content(student.name)
+      expect(page).to have_content(student2.name)
+
     end
   end
 end
