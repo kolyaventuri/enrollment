3c5ac12cf1d50a3b22547bd820e5c795d471939c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 14:23:28 -0600

    Address spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe Student, type: :model do
-  it { is_expected.to validate_presence_of(:name) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe 'relationships' do
+    it { is_expected.to have_many(:addresses) }
+  end
 end
