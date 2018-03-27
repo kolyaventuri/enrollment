deaa6c4b395e28da73a271659844fb3a82667e55
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 15:45:08 -0600

    Check that students only have their own courses

--- spec/features/courses/user_can_see_students_courses_spec.rb
+++ spec/features/courses/user_can_see_students_courses_spec.rb
@@ -26,4 +26,24 @@ describe 'User' do
       expect(page).to have_content(course2.name)
     end
   end
+
+  describe 'visits another students page' do
+    it 'should be able to have only that students courses' do
+      student = Student.create!(name: 'Bob')
+      student2 = Student.create!(name: 'Bob')
+      course1 = Course.create!(code: 'EEE100', name: 'Intro to Electrical Engineering')
+      course2 = Course.create!(code: 'ENG101', name: 'College Writing')
+      course3 = Course.create!(code: 'MAT202', name: 'College Math')
+      StudentCourse.create!(student_id: student.id, course_id: course1.id)
+      StudentCourse.create!(student_id: student.id, course_id: course2.id)
+      StudentCourse.create!(student_id: student2.id, course_id: course3.id)
+
+      visit student_path(student2)
+
+      expect(page).to have_content(course3.code)
+      expect(page).to have_content(course3.name)
+
+      expect(page).to_not have_content(course1.code)
+    end
+  end
 end
