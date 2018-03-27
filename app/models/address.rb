1ec27b8e1707fc29a05afe38794d3562101d54c6
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 14:24:16 -0600

    Address model and student-address relationship

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ApplicationRecord
+  validates_presence_of :description
+  validates_presence_of :street
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
 
+  belongs_to :student
 end
