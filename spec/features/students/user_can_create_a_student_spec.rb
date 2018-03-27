88306e8e85bc33cf3d9923cb0eeed7751d2fbb92
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 14:01:47 -0600

    New student test

---
+++ spec/features/students/user_can_create_a_student_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+def random_name
+  rand(36**8).to_s(36)
+end
+
+describe 'User' do
+  before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  describe 'visits the new user page' do
+    it 'should alow them to create a new student' do
+      visit new_student_path
+      name = random_name
+
+      fill_in 'student[name]', with: name
+
+      click_on 'Create Student'
+
+      expect(current_path).to eq('/students/1')
+
+      expect(page).to have_content(name)
+    end
+  end
+end
