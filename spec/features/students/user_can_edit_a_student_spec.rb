860bfa2ed458273287c7acf5ca532d020e8b413c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-27 13:42:11 -0600

    Update student test

---
+++ spec/features/students/user_can_edit_a_student_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'User' do
+  before(:all) do
+    DatabaseCleaner.clean
+  end
+
+  after(:all) do
+    DatabaseCleaner.clean
+  end
+
+  describe 'visits a student edit page' do
+    it 'should be able to update a student' do
+      student = Student.create!(name: 'Bob')
+      new_name = 'Megan'
+
+      visit edit_student_path(student)
+
+      expect(page).to have_content(student.name)
+
+      fill_in 'student[name]', with: new_name
+      click_on 'Update Student'
+
+      expect(current_path).to eq(student_path(student))
+      expect(page).to have_content(new_name)
+    end
+  end
+end
